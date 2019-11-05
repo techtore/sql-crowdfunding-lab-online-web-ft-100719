86445a17aa1fed473b52651ff05e1bf323a04a33
Victoria Hale <vic.hale74@gmail.com>
2019-11-05 21:01:50 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO projects(title) VALUES ("project_1", "project_2", "project_3", "project_4", "project_5", "project_6", "project_7", "project_8", "project_9", "project_10");
+INSERT INTO projects(title) VALUES ();
