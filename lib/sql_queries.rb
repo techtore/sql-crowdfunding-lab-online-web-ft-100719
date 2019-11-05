d821f0c85514e6f53d084633d39d002c952cbc15
Victoria Hale <vic.hale74@gmail.com>
2019-11-05 21:19:05 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,7 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  "SELECT projects.title.* 
+  "SELECT projects.title 
    FROM projects 
    INNER JOIN pledges 
    ON projects.title = pledges.amount";
