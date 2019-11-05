2f7a11ea7ae18730ccc94f3ab11c3f8685d6a010
Victoria Hale <vic.hale74@gmail.com>
2019-11-05 21:26:47 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT projects.title, SUM (pledges.amount) FROM projects
-    INNER JOIN projects_title 
+    INNER JOIN projectstitle 
     ON pledges.product_id = projects.id 
     GROUP BY projects.title;"
 end
