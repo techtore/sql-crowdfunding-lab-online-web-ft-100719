7723b5c2f257b2c5129181f36392b92b03407ce4
Victoria Hale <vic.hale74@gmail.com>
2019-11-05 20:52:30 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,8 +1,6 @@
-CREATE TABLE projects (id INTEGER, title TEXT, category TEXT, funding_goal REAL, start_date TEXT, end_date TEXT
-);
+CREATE TABLE projects (id INTEGER, title TEXT, category TEXT, funding_goal REAL, start_date TEXT, end_date TEXT);
 
-CREATE TABLE users (
-);
+CREATE TABLE users (id INTEGER, name TEXT, age INTEGER);
 
 CREATE TABLE pledges (
 );
