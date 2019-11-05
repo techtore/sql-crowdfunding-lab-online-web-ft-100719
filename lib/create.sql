6890aff66459acb4c63c385d52ad0eb67f3148df
Victoria Hale <vic.hale74@gmail.com>
2019-11-05 20:52:59 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,5 +2,4 @@ CREATE TABLE projects (id INTEGER, title TEXT, category TEXT, funding_goal REAL,
 
 CREATE TABLE users (id INTEGER, name TEXT, age INTEGER);
 
-CREATE TABLE pledges (
-);
+CREATE TABLE pledges (id INTEGER, amount REAL, user_id INTEGER, project_id INTEGER);
