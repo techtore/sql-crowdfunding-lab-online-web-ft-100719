dfe16039cae15c3acb7efff66901d384a8d64767
Victoria Hale <vic.hale74@gmail.com>
2019-11-05 21:18:29 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT title.* 
    FROM projects 
-   INNER JOIN pldeges 
+   INNER JOIN pledges 
    ON projects.title = pledges.amount";
 end
 
